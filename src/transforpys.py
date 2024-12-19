3f5cfa8bf362e5fb394e199907994b1fc61f01a7
muctebakarabey1 <muctebakarabey12@gmail.com>
2024-12-19 03:19:10 -0900

    new bversion 3.1

--- src/transforpys.py
+++ src/transforpys.py
@@ -1,6 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when, lit
-from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, when, lit, avg
 from pyspark.sql.functions import col, lit, when, sum, avg
 from pyspark.sql.functions import sum, format_number
